886e63fabad20e02c8936e0e8715bd1b58311c10
No1zy <bgkn12601208@gn.iwasaki.ac.jp>
2019-10-14 19:03:11 +0900

    Delete unnecessary code

--- src/main/java/breakscripttag/BreakScriptTag.java
+++ src/main/java/breakscripttag/BreakScriptTag.java
@@ -2,17 +2,13 @@ package breakscripttag;
 
 import burp.IBurpExtender;
 import burp.IBurpExtenderCallbacks;
-import burp.IExtensionHelpers;
 
 public class BreakScriptTag implements IBurpExtender {
     private IBurpExtenderCallbacks callbacks;
-    private IExtensionHelpers helpers;
-
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         this.callbacks = callbacks;
-        this.helpers = callbacks.getHelpers();
 
         callbacks.setExtensionName("Broken Script tag injection");
 
